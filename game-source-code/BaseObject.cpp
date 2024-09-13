2c2162680432f4f3d1408e55d272d57606be0bc7
Yasteel Ramphal <yasteel001@gmail.com>
2024-09-13 18:23:35 +0200

    Base Object class implementation

---
+++ game-source-code/BaseObject.cpp
@@ -0,0 +1,54 @@
+#include "BaseObject.h"
+
+using namespace std; 
+
+BaseObject::BaseObject(int x_pos, int y_pos){
+    current_x = x_pos;
+    current_y = y_pos;
+    object_Properties = {false,false,false,false,false,false};
+    sprite = "";
+    cout <<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+    
+}
+
+Properties BaseObject::get_Properties(){
+    return object_Properties;
+};
+
+string BaseObject::get_sprite(){
+    return sprite;
+};
+
+void BaseObject::set_Properties(Properties new_Properties){
+    object_Properties = new_Properties;
+};
+
+void BaseObject::set_sprite(string sprite_file_location){
+    sprite = sprite_file_location;
+};
+
+void BaseObject::set_location(int x, int y){
+    current_x = x;
+    current_y = y;
+};
+
+
+int BaseObject::get_x(){
+    return current_x;
+};
+
+int BaseObject::get_y(){
+    return current_y;
+};
+
+void BaseObject::set_moved( bool moved){
+    object_Properties.is_moved = moved;
+};
+
+void BaseObject::set_window(shared_ptr<raylib::Window> window){
+    window_ = window;
+};
+
+shared_ptr<raylib::Window> BaseObject::get_window(){
+    return window_;
+};
