15422a339b5ac9ed0421bb28254490ff3119b1b4
Yasteel Ramphal <yasteel001@gmail.com>
2024-09-13 19:16:30 +0200

    minor fixes to scope of functions

--- game-source-code/BaseObject.h
+++ game-source-code/BaseObject.h
@@ -25,24 +25,19 @@ class BaseObject {
     void set_Properties(Properties new_Properties);
     int get_x();
     int get_y();
-    int get_lives();
-    int get_points();
-    void set_points(int value);
     virtual void set_location(int x, int y);
     virtual string get_sprite();
     void set_sprite(string sprite_file_location);
-    //void set_window(shared_ptr<sf::RenderWindow> window);
+    void set_window(shared_ptr<raylib::Window> window);
     void set_moved(bool moved);
-    shared_ptr<raylib::window> get_window();
+    shared_ptr<raylib::Window> get_window();
 
 
     protected:
         int current_x;
         int current_y;
-        int lives;
     private:
-        shared_ptr<sf::RenderWindow> window_;
-        int point_value;
+        shared_ptr<raylib::Window> window_;
         string sprite;
         Properties object_Properties;
 
