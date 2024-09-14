c0a2dcf351cd8f3791509720b07a797de8bc7603
Matthew Green <matthewgreen608@gmail.com>
2024-09-14 13:19:17 +0200

    Fixing commits

---
+++ game-source-code/GameMap.h
@@ -0,0 +1,26 @@
+#ifndef BEB9148E_1DD3_4978_8B60_98C49B9FE9F8
+#define BEB9148E_1DD3_4978_8B60_98C49B9FE9F8
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <raylib-cpp.hpp>
+
+using namespace std;
+
+class GameMap
+{
+    public:
+        GameMap();
+        void DrawMap();
+        void DrawBoundary();
+        //void DrawGhostSpawn();
+    private:
+        vector<vector<int>> MapObjects;
+        int NumOfRectangles=0;
+        int NumOfLines=0;
+};
+
+
+#endif /* BEB9148E_1DD3_4978_8B60_98C49B9FE9F8 */
