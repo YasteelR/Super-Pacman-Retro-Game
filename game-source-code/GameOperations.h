6dd065b20009aa82712e3698986adf99ec9ddb40
Yasteel Ramphal <yasteel001@gmail.com>
2024-09-14 15:23:08 +0200

    added Game operations class definitions

---
+++ game-source-code/GameOperations.h
@@ -0,0 +1,29 @@
+#ifndef CE167A85_A911_4D91_8B62_622EFB213774
+#define CE167A85_A911_4D91_8B62_622EFB213774
+
+#include<string>
+#include <iostream>
+#include "player.h"
+
+class GameOperations{
+public:
+    GameOperations();
+    void Start();
+    //void Execute_Game_Itteration();
+    //void set_Game_Info(const int& score,const int& lives);
+    void move_objects();
+    static bool is_game_over;
+
+
+
+    // put in seporate class
+    void drawPlayer();
+private:
+    shared_ptr<player> pacman;
+
+    //unique_ptr<Setup> initialise_game;
+    //unique_ptr<GameItteration> game_round_;
+    //shared_ptr<GameData> game_data;
+
+};
+#endif /* CE167A85_A911_4D91_8B62_622EFB213774 */
