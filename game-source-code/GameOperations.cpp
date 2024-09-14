858d13ee89fccdc17f1692715ac57835e1797533
Yasteel Ramphal <yasteel001@gmail.com>
2024-09-14 15:23:38 +0200

    added Game operations class implementation

---
+++ game-source-code/GameOperations.cpp
@@ -0,0 +1,65 @@
+#include "GameOperations.h"
+
+GameOperations::GameOperations(){
+    pacman = make_shared<player>(player(1800,700));
+
+
+}
+
+void GameOperations::move_objects(){
+
+    if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_S) || IsKeyPressed(KEY_A) || IsKeyPressed(KEY_D)) {
+            // Get the key input using a switch statement
+            switch (GetKeyPressed()) {
+                case KEY_W:
+                    // Move Up
+                    pacman->moveUp();
+                    break;
+                case KEY_S:
+                    // Move Down
+                    pacman->moveDown();
+                    break;
+                case KEY_A:
+                    // Move Left
+                    pacman->moveLeft();
+                    break;
+                case KEY_D:
+                    // Move Right
+                    pacman->moveRight();
+                    break;
+                default:
+                    break;
+            }
+
+    }
+    
+    if(IsKeyDown(KEY_UP)) {
+        pacman->moveUp();
+    }
+    else if(IsKeyDown(KEY_DOWN)) {
+        pacman->moveDown();
+    }
+    if(IsKeyDown(KEY_RIGHT)) {
+        pacman->moveRight();
+    }
+    else if(IsKeyDown(KEY_LEFT)) {
+        pacman->moveLeft();
+    }
+}
+
+
+void GameOperations::drawPlayer(){
+
+    BeginDrawing();
+
+    
+    //ClearBackground(BLUE);
+    pacman->draw_sprite_object();
+
+    //PacMan_Window->drawPlayer();
+    // End drawing
+    //EndDrawing();
+    
+    cout<<"drawing pacman++++++++++++++"<<endl;
+
+}
