0f0d34eecb1c28f4bec3526f748fd555146978f6
Yasteel Ramphal <yasteel001@gmail.com>
2024-09-15 18:15:32 +0200

    collision for down

--- game-source-code/player.cpp
+++ game-source-code/player.cpp
@@ -20,12 +20,56 @@ void player::moveUp(){
     }
 
 void player::moveDown() {
-         if (get_y() < 755) {
+
+    if (get_y() < 240){
+        if (get_y() < 800) {
+        set_location(get_x(), get_y() + 15);
+    }
+
+    } else if ( get_y() > 225 && get_y() < 495 ){
+         if (get_y() <800) {
+            set_location(get_x(), get_y() + 15);
+        }
+    } else if ( get_x() > 1340 ){
+        if (get_y() <800) {
+        set_location(get_x(), get_y() + 15);
+    }    
+    }else if (get_x() < 215){
+         if (get_y() < 800) {
             set_location(get_x(), get_y() + 15);
         }
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //  if (get_y() < 700) {
+        //     set_location(get_x(), get_y() + 15);
+        // }
+}
+
 void player::moveLeft() {
+
+
+
+
+
+
+
+
+
+
+    
          if (get_x() > 5) {
             set_location(get_x() - 15, get_y() );
             //set_sprite("resources/pacmanLeft.png");
@@ -40,13 +84,13 @@ void player::moveRight() {
             cout<<"Phase 1 "<<endl;
         }
 
-    } else if ( get_x() > 200 && get_y() > 240 && get_y() < 465 ){
+    } else if ( get_x() > 200 && get_y() > 240 && get_y() < 500 ){
         if (get_x() < 1500) {
             set_location(get_x() + 15, get_y() );
             //set_sprite("resources/pacmanRight.png");
             cout<<"Phase 2 "<<endl;
         }
-    }else if (get_x() >1400){
+    }else if (get_x() >1340){
         if (get_x() < 1500) {
             set_location(get_x() + 15, get_y() );
             //set_sprite("resources/pacmanRight.png");
