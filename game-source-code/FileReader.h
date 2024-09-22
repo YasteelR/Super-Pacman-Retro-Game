83ea8731059da9181dd825dee0bfe508b1939b0f
Matthew Green <matthewgreen608@gmail.com>
2024-09-22 09:13:23 +0200

    Refactored code

--- game-source-code/FileReader.h
+++ game-source-code/FileReader.h
@@ -3,21 +3,27 @@
 
 #include <fstream>
 #include <string>
+#include <vector>
+
+using namespace std;
 
 class FileReader
 {
     public:
-        FileReader();
+        FileReader(string ReadInFile);
 
-        //Reads the Map file
         void SetMapFile(string& FileName);
         void OpenFile();
         void OpenFile(string& FileName);//Overload OpenFile to take in a file name
         void CloseFile();
         bool FileIsOpen();
-        void ReadData();
+
+        //Reads the Map txt file.
+        void ReadData(vector<int>& StoreData, int& NumberOfObjects);
 
     private:
         string MapFile;
-}
+        ifstream InputFile;
+
+};
 #endif /* B5A97AA2_DF14_43C3_88A9_C49E15AF5231 */
