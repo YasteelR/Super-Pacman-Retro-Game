803b2891007230e78b06006958d2a212acad834f
Matthew Green <matthewgreen608@gmail.com>
2024-09-25 06:49:22 +0200

    Idea for GameData

--- game-source-code/GameData.h
+++ game-source-code/GameData.h
@@ -11,14 +11,19 @@ class GameData
     public:
         GameData();
         void ReadInWalls(string FilePath);
-        vector<int>* getWallCoordinates();
+        vector<int>* getWallCoordinates();//Returns a pointer to the vector of wall coordinates (so their positions can be read in)
         int* getNumberOfWalls();
+        void setGhosts(); //sets the number of ghosts
+        vector<int>* getGhostCoordinates();//Returns a pointer to the ghosts (so their positions can be updated)
 
 
     private:
         vector<BaseObject> Object;
         vector<int> WallCoordinates;
         int NumberOfWalls;
+        vector<int> PlayerCoordinates;
+        vector<int> GhostCoordinates;
+        int NumOfGhosts;
 
 };
 
