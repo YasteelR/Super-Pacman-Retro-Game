09d40be9362390c601b3d5b5c96747cb222b2126
Matthew Green <matthewgreen608@gmail.com>
2024-09-26 13:35:39 +0200

    Added to Star

--- game-source-code/Star.cpp
+++ game-source-code/Star.cpp
@@ -2,6 +2,18 @@
 
 Star::Star()
 {
-    png1.set_sprite("../resources/banana.png");
-    png2.set_sprite("../resources/orange.png");
+    set_sprite("../resources/star.png");
+    starFruits.emplace_back();
+    starFruits.emplace_back();
+    
+    fruitsFilepath.emplace_back("../resources/banana.png");
+    fruitsFilepath.emplace_back("../resources/oranges.png");
+    fruitsFilepath.emplace_back("../resources/cherry.png");
+}
+
+void Star::setObjects()
+{
+    starFruits[0].set_sprite();
+    starFruits[1].set_sprite();
+    starFruits[2].set_sprite();
 }
