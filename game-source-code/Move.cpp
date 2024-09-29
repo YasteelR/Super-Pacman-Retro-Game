be1f345d1c38f402d7a1d0c1210b31ea1b3c374b
Matthew Green <matthewgreen608@gmail.com>
2024-09-29 18:15:16 +0200

    Added move.cpp

---
+++ game-source-code/Move.cpp
@@ -0,0 +1,3 @@
+#include "Move.h"
+
+Move::Move(int x, int y) : BaseObject(x,y){}
--- game-source-code/Render.h
+++ game-source-code/Render.h
@@ -1,7 +1,5 @@
 #ifndef AF83D00B_46A7_46C7_8815_82219216F383
 #define AF83D00B_46A7_46C7_8815_82219216F383
-#ifndef B26D77E5_104C_4214_BAD4_3833169DA82E
-#define B26D77E5_104C_4214_BAD4_3833169DA82E
 
 #include <vector>
 #include <memory>
@@ -52,8 +50,5 @@ void Render::drawObjects(shared_ptr<T> Object)
 {
     Object->draw_sprite_object();
 }
-
-#endif /* B26D77E5_104C_4214_BAD4_3833169DA82E */
-
 
 #endif /* AF83D00B_46A7_46C7_8815_82219216F383 */
