5ad66c035e8512d0e0d53fc9efdac622b8e204cd
Matthew Green <matthewgreen608@gmail.com>
2024-09-29 16:31:33 +0200

    Refactored Star

--- game-source-code/Star.h
+++ game-source-code/Star.h
@@ -15,19 +15,19 @@ class Star : public BaseObject
         void changeImages();
         void DrawCompanions();
         void setCompanionPosition();
-        void createCompanions();
-        void setUpCompanions();
+        void createCompanions(int& companions);
+        void setUpCompanions(int companions);
         bool CompanionsMatch();
         bool CompanionsMatchFruit();
         void Destroy();
-
-        void move_Obj();
-        void undoLastMove();
+        shared_ptr<vector<shared_ptr<BaseObject>>> getCompanions();
+        void setTimer(int duration);
 
     private:
-        vector<shared_ptr<Star>> starFruits;
+        vector<shared_ptr<BaseObject>> starFruits;
         vector<string> fruitsFilepath;
         int clock;
+        int timer;
 };
 
 #endif /* BB5257F6_5AD4_4BE3_9A6C_BB94C19F6F37 */
