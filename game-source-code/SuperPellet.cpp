b16eaa9bd0848da88cbef1c9d88c27c294d49900
Matthew Green <matthewgreen608@gmail.com>
2024-10-03 12:25:38 +0200

    Added SuperPellet

---
+++ game-source-code/SuperPellet.cpp
@@ -0,0 +1,31 @@
+#include "SuperPellet.h"
+
+SuperPellet::SuperPellet() : Sprite()
+{
+    set_sprite("../resources/powerPellet.png");
+    active=0;
+}
+
+void SuperPellet::duration()
+{
+    if(active>0)
+    {
+        active--;
+    }
+}
+
+bool SuperPellet::activePower()
+{
+    if(active>0)
+    {
+        return true;
+    }
+    else 
+        return false;
+}
+
+void SuperPellet::activate()
+{
+    active=450;
+    set_location(-100,-100);
+}
