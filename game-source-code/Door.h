422705f12a67c46a9bf5e0982ab01fa82f8453f0
Yasteel Ramphal <yasteel001@gmail.com>
2024-10-09 18:29:40 +0200

    documentation for door

--- game-source-code/Door.h
+++ game-source-code/Door.h
@@ -1,24 +1,57 @@
 #ifndef C98DA9BF_020E_42CF_BEB5_1BAAFD9D9AEC
 #define C98DA9BF_020E_42CF_BEB5_1BAAFD9D9AEC
+
 #include "BaseObject.h"
 #include "raylib-cpp.hpp"
 #include <iostream>
 
 using namespace std;
 
-class Door : public BaseObject{
+/**
+ * @brief Represents a Door object in the game.
+ * 
+ * The Door class provides functionality for door objects,
+ * including getting and setting dimensions.
+ */
+class Door : public BaseObject {
 public:
-
     Door();
+    /**
+     * @brief Gets the width of the door.
+     * 
+     * @return int The width of the door.
+     */
     int getWidth();
+    
+    /**
+     * @brief Gets the height of the door.
+     * 
+     * @return int The height of the door.
+     */
     int getHeight();
+    
+    /**
+     * @brief Sets the width of the door.
+     * 
+     * @param width_ The new width of the door.
+     */
     void setWidth(int width_);
+    
+    /**
+     * @brief Sets the height of the door.
+     * 
+     * @param height_ The new height of the door.
+     */
     void setHeight(int height_);
+    
+    /**
+     * @brief Destroys the door by resetting its position and dimensions.
+     */
     void destroy();
 
 private:
-    int width;
-    int height;
+    int width;  ///< The width of the door.
+    int height; ///< The height of the door.
 };
 
 #endif /* C98DA9BF_020E_42CF_BEB5_1BAAFD9D9AEC */
