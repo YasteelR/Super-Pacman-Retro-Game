d90df0d68a8f148474742ec854270ea50711c6ba
Matthew Green <matthewgreen608@gmail.com>
2024-10-09 20:24:45 +0200

    Added comments

--- game-source-code/SuperPellet.cpp
+++ game-source-code/SuperPellet.cpp
@@ -1,41 +1,48 @@
 #include "SuperPellet.h"
+
+// Static member variable to track the number of active super pellets.
 int SuperPellet::Super = 0;
 
+// Constructor that initializes the SuperPellet object and its properties.
 SuperPellet::SuperPellet() : Sprite()
 {
-    set_sprite("../resources/superPellet.png");
-    active=0;
-    Super=0;
-    powerful = false;
+    set_sprite("../resources/superPellet.png"); // Set the sprite for the super pellet.
+    active = 0; // Initialize the active state to 0 (not active).
+    Super = 0; // Reset the count of active super pellets.
+    powerful = false; // Set the powerful state to false.
 }
 
+// Decreases the active duration of the super pellet and manages its powerful state.
 void SuperPellet::duration()
 {
-    if(active>0)
+    if(active > 0) // Check if the super pellet is currently active.
     {
-        active--;
+        active--; // Decrease the active duration.
     }
-    else if (active==0&&powerful)
+    else if (active == 0 && powerful) // If active duration has expired and it's powerful.
     {
-        Super--;
-        powerful=false;
+        Super--; // Decrease the count of active super pellets.
+        powerful = false; // Set powerful state to false.
     }
 }
 
+// Checks if there is an active super pellet power.
 bool SuperPellet::activePower()
 {
-    if(Super>0)
+    // Return true if there are active super pellets.
+    if(Super > 0)
     {
-        return true;
+        return true; // Power is active.
     }
     else 
-        return false;
+        return false; // No active power.
 }
 
+// Activates the super pellet, setting it to active state and updating its properties.
 void SuperPellet::activate()
 {
-    active=450;
-    powerful = true;
-    Super++;
-    set_location(-100,-100);
+    active = 450; // Set the active duration.
+    powerful = true; // Mark the super pellet as powerful.
+    Super++; // Increment the count of active super pellets.
+    set_location(-100, -100); // Move the pellet out of view.
 }
