8d26dd2842c49319be3696dfc55610ffc9e4aaac
Yasteel Ramphal <yasteel001@gmail.com>
2024-10-09 18:32:07 +0200

    documentation for fruit

--- game-source-code/Fruit.h
+++ game-source-code/Fruit.h
@@ -3,19 +3,39 @@
 
 #include "Sprite.h"
 
-using namespace std;
+/**
+ * @brief Represents a Fruit object in the game.
+ * 
+ * The Fruit class manages fruit objects, including their state and actions.
+ */
+class Fruit : public Sprite {
+public:
+    Fruit();
+    
+    /**
+     * @brief Gets the number of fruits.
+     * 
+     * @return int The current number of fruits.
+     */
+    int getFruitNum();
+    
+    /**
+     * @brief Handles the event of a fruit being eaten.
+     */
+    static void eatenFruit();
+    
+    /**
+     * @brief Destroys the fruit by resetting its position.
+     */
+    void Destroy();
+    
+    /**
+     * @brief Resets the fruit count.
+     */
+    static void reset();
 
-class Fruit : public Sprite
-{
-    public:
-        Fruit();
-        int getFruitNum();
-        static void eatenFruit();
-        void Destroy();
-        static void reset();
-
-    private:
-        static int NumOfFruit;
+private:
+    static int NumOfFruit; ///< The total number of fruits.
 };
 
 #endif /* A4EC8B3D_2578_488A_8DDC_D51A5CCCD2FC */
