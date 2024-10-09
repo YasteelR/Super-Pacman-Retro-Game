88dcf60c33cb9659f9904ce6031d49eb78fde518
Matthew Green <matthewgreen608@gmail.com>
2024-10-09 20:26:38 +0200

    Added comments

--- game-source-code/Wall.cpp
+++ game-source-code/Wall.cpp
@@ -1,24 +1,28 @@
 #include "Wall.h"
 
+// Constructor that initializes the Wall object by calling the base class constructor.
+Wall::Wall() : BaseObject() {}
 
-Wall::Wall() : BaseObject(){}
-
+// Returns the width of the wall.
 int Wall::getWidth()
 {
-    return width;
+    return width; // Return the width of the wall.
 }
 
+// Returns the height of the wall.
 int Wall::getHeight()
 {
-    return height;
+    return height; // Return the height of the wall.
 }
 
+// Sets the height of the wall to a specified value.
 void Wall::setHeight(int height_)
 {
-    height=height_;
+    height = height_; // Update the height with the provided value.
 }
 
+// Sets the width of the wall to a specified value.
 void Wall::setWidth(int width_)
 {
-    width=width_;
+    width = width_; // Update the width with the provided value.
 }
