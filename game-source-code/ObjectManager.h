ae58a3e56abb9e312f9605b9ea100d0f6d59909c
Yasteel Ramphal <yasteel001@gmail.com>
2024-10-09 18:54:55 +0200

    documentation for obj manager

--- game-source-code/ObjectManager.h
+++ game-source-code/ObjectManager.h
@@ -11,81 +11,79 @@
 #include "Wall.h"
 #include "PowerPellet.h"
 
-class ObjectManager
-{
-    public:
-        ObjectManager(){};
-        void loadObjects(string filepath);
-        template <typename T>
-        shared_ptr<T> get(string type);
-    private:
-        FileReader ObjectFile;
+/**
+ * @brief Manages game objects and their interactions.
+ * 
+ * The ObjectManager class handles the loading and retrieval of game objects.
+ */
+class ObjectManager {
+public:
+    ObjectManager() {};
+    
+    /**
+     * @brief Loads objects from the specified file.
+     * 
+     * @param filepath The path to the file containing object data.
+     */
+    void loadObjects(string filepath);
+    
+    /**
+     * @brief Retrieves an object by its type.
+     * 
+     * @tparam T The type of the object.
+     * @param type The type of object to retrieve.
+     * @return shared_ptr<T> The requested object.
+     */
+    template <typename T>
+    shared_ptr<T> get(string type);
 
-        shared_ptr<player> playerPacman;
-        vector<shared_ptr<Ghost>> ghosts;
-        vector <shared_ptr<Wall>> walls;
-        vector <shared_ptr<Wall>> boundaries;
-        vector<shared_ptr<Key>> keys;
-        vector<shared_ptr<Door>> doors;
-        vector<shared_ptr<Fruit>> fruits;
-        shared_ptr<Score> scores;
-        vector<shared_ptr<Star>> stars;
-        vector<shared_ptr<PowerPellet>> powerpellets;
+private:
+    FileReader ObjectFile; ///< FileReader for object data.
+    shared_ptr<player> playerPacman; ///< The player object.
+    vector<shared_ptr<Ghost>> ghosts; ///< Vector of ghost objects.
+    vector <shared_ptr<Wall>> walls; ///< Vector of wall objects.
+    vector <shared_ptr<Wall>> boundaries; ///< Vector of boundary objects.
+    vector<shared_ptr<Key>> keys; ///< Vector of key objects.
+    vector<shared_ptr<Door>> doors; ///< Vector of door objects.
+    vector<shared_ptr<Fruit>> fruits; ///< Vector of fruit objects.
+    shared_ptr<Score> scores; ///< Score object.
+    vector<shared_ptr<Star>> stars; ///< Vector of star objects.
+    vector<shared_ptr<PowerPellet>> powerpellets; ///< Vector of power pellet objects.
 };
 
 template <typename T>
-shared_ptr<T> ObjectManager::get(string type)
-{
-    if(type == "player")
-    {
+shared_ptr<T> ObjectManager::get(string type) {
+    if (type == "player") {
         return playerPacman;
-    }
-    else if(type == "Ghost")
-    {
+    } else if (type == "Ghost") {
         auto ghostPointer = make_shared<vector<shared_ptr<Ghost>>>(ghosts);
         return ghostPointer;
-    }
-    else if(type == "Wall")
-    {
+    } else if (type == "Wall") {
         auto wallPointer = make_shared<vector<shared_ptr<Wall>>>(walls);
         return wallPointer;
-    }
-    else if(type == "Boundary")
-    {
+    } else if (type == "Boundary") {
         auto boundaryPointer = make_shared<vector<shared_ptr<Wall>>>(boundaries);
         return boundaryPointer;
-    }
-    else if(type == "Key")
-    {
+    } else if (type == "Key") {
         auto keyPointer = make_shared<vector<shared_ptr<Key>>>(keys);
         return keyPointer;
-    }
-    else if(type == "Door")
-    {
+    } else if (type == "Door") {
         auto doorPointer = make_shared<vector<shared_ptr<Door>>>(doors);
         return doorPointer;
-    }
-    else if(type == "Fruit")
-    {
+    } else if (type == "Fruit") {
         auto fruitPointer = make_shared<vector<shared_ptr<Fruit>>>(fruits);
         return fruitPointer;
-    }
-    else if(type == "Score")
-    {
+    } else if (type == "Score") {
         return scores;
-    }
-    else if(type == "Star")
-    {
+    } else if (type == "Star") {
         auto starPointer = make_shared<vector<shared_ptr<Star>>>(stars);
         return starPointer;
-    }
-    else if(type == "PowerPellet")
-    {
+    } else if (type == "PowerPellet") {
         auto powerpelletsPointer = make_shared<vector<shared_ptr<PowerPellet>>>(powerpellets);
         return powerpelletsPointer;
-    }
-    else 
+    } else {
         throw runtime_error("Type does not match any objects");
+    }
 }
 
 #endif /* DCFC5681_4E68_4E0A_AF25_1C9FDF03378B */
