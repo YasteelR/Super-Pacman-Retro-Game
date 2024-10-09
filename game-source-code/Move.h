21105787e3742a2f19b8517310e98ec11cb229f2
Yasteel Ramphal <yasteel001@gmail.com>
2024-10-09 18:52:53 +0200

    documentation for move

--- game-source-code/Move.h
+++ game-source-code/Move.h
@@ -3,12 +3,32 @@
 
 #include "Sprite.h"
 
-class Move : public Sprite
-{
-    public:
-        Move(int x, int y);
-        virtual void move_Obj() = 0;
-        virtual void undoLastMove() = 0;
+/**
+ * @brief Abstract base class for movable objects.
+ * 
+ * The Move class provides an interface for moving and undoing moves.
+ */
+class Move : public Sprite {
+public:
+    /**
+     * @brief Constructor with position parameters.
+     * 
+     * Initializes the move object at specified coordinates.
+     * 
+     * @param x The initial x-coordinate.
+     * @param y The initial y-coordinate.
+     */
+    Move(int x, int y);
+    
+    /**
+     * @brief Pure virtual function to move the object.
+     */
+    virtual void move_Obj() = 0;
+    
+    /**
+     * @brief Pure virtual function to undo the last move.
+     */
+    virtual void undoLastMove() = 0;
 };
 
 #endif /* B01A566E_8EE6_4DF0_8642_E48F2AD1AC0A */
