d25cee3db77b52f31fe95e1e516809c8ad571ba6
Yasteel Ramphal <yasteel001@gmail.com>
2024-10-09 18:27:02 +0200

    document for collisions

--- game-source-code/Collisions.h
+++ game-source-code/Collisions.h
@@ -8,42 +8,72 @@
 
 using namespace std;
 
-class Collisions
-{
-    public:
-        Collisions(){};
-        template <typename T1, typename T2>
-        void checkCollisions(vector<shared_ptr<T1>> Object, shared_ptr<T2> Player);
+/**
+ * @brief Handles collision detection between game objects.
+ * 
+ * The Collisions class provides methods for checking collisions
+ * between different objects in the game.
+ */
+class Collisions {
+public:
+    Collisions() {};
+    /**
+     * @brief Checks for collisions between two sets of objects.
+     * 
+     * @tparam T1 Type of the first object.
+     * @tparam T2 Type of the second object.
+     * @param Object Vector of shared pointers to objects.
+     * @param Player Shared pointer to the player object.
+     */
+    template <typename T1, typename T2>
+    void checkCollisions(vector<shared_ptr<T1>> Object, shared_ptr<T2> Player);
+    
+    /**
+     * @brief Gets the collision status.
+     * 
+     * @return true if a collision occurred, false otherwise.
+     */
+    bool getCollision();
+    
+    /**
+     * @brief Gets the index of the colliding object.
+     * 
+     * @return int The index of the colliding object.
+     */
+    int getObject();
+    
+    /**
+     * @brief Resets the collision status.
+     */
+    void resetCollision();
 
-        bool getCollision();
-        int getObject();
-        void resetCollision();
-        
-        template <typename T>
-        Rectangle returnRect(shared_ptr<T> Object);
-        
+    /**
+     * @brief Returns the bounding rectangle of an object.
+     * 
+     * @tparam T Type of the object.
+     * @param Object Shared pointer to the object.
+     * @return Rectangle The bounding rectangle of the object.
+     */
+    template <typename T>
+    Rectangle returnRect(shared_ptr<T> Object);
 
-    private:
-        tuple<bool,int> CollisionObject;
+private:
+    tuple<bool, int> CollisionObject; ///< Stores collision state and object index.
 };
 
 template <typename T1, typename T2>
-void Collisions::checkCollisions(vector<shared_ptr<T1>> Object, shared_ptr<T2> Player)
-{
-    for(int i=0; i<Object.size(); i++)
-    {
-        if(CheckCollisionRecs(returnRect(Object[i]), Player->getBoundingBox()))
-        {
-            get<0>(CollisionObject)=true;
-            get<1>(CollisionObject)=i;
+void Collisions::checkCollisions(vector<shared_ptr<T1>> Object, shared_ptr<T2> Player) {
+    for (int i = 0; i < Object.size(); i++) {
+        if (CheckCollisionRecs(returnRect(Object[i]), Player->getBoundingBox())) {
+            get<0>(CollisionObject) = true;
+            get<1>(CollisionObject) = i;
             break;
         }
     }
 }
 
 template <typename T>
-Rectangle Collisions::returnRect(shared_ptr<T> Object)
-{
+Rectangle Collisions::returnRect(shared_ptr<T> Object) {
     return {(float)Object->get_x(), (float)Object->get_y(), (float)Object->getWidth(), (float)Object->getHeight()};
 }
 
