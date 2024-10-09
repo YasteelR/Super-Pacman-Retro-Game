85905cc62656066fbafc9b4874a619d5157cd2b1
Yasteel Ramphal <yasteel001@gmail.com>
2024-10-09 19:29:00 +0200

    documentation for s pellet

--- game-source-code/SuperPellet.h
+++ game-source-code/SuperPellet.h
@@ -7,17 +7,35 @@
 
 using namespace std;
 
-class SuperPellet : public Sprite{
+/**
+ * @brief Represents a Super Pellet object in the game.
+ * 
+ * The SuperPellet class manages super pellet behavior and state.
+ */
+class SuperPellet : public Sprite {
 public:
-
     SuperPellet();
+    
+    /**
+     * @brief Updates the duration of the super pellet.
+     */
     void duration();
+    
+    /**
+     * @brief Checks if the super pellet is active.
+     * 
+     * @return true if active, false otherwise.
+     */
     bool activePower();
+    
+    /**
+     * @brief Activates the super pellet.
+     */
     void activate();
 
 private:
-    int active;
-    int PelletNumber;
+    int active; ///< Active duration of the super pellet.
+    int PelletNumber; ///< The pellet's identifier.
 };
 
-#endif /* D3835E53_B072_4ADE_AC8E_A6F35E290691 */
+#endif /* DADBC4C0_964C_40F7_98D7_993A0DD45080 */
