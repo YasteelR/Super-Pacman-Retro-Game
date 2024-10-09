e8108cc67d84e54ed11ac2a26a433d74460920b7
Matthew Green <matthewgreen608@gmail.com>
2024-10-09 19:14:17 +0200

    Added doxygen

--- game-source-code/AssetLoader.h
+++ game-source-code/AssetLoader.h
@@ -19,7 +19,14 @@ class AssetLoader
     private:
         FileReader ObjectFile;
 };
+/// Implementation of template methods
 
+/**
+ * @brief Template specialization for loading a vector of shared_ptr objects.
+ * @tparam T The type of the objects being loaded.
+ * @param Object A vector of shared pointers to objects of type T.
+ * @param ObjectName The name of the object to be loaded.
+ */
 template <typename T>
 void AssetLoader::LoadObject(vector<shared_ptr<T>>& Object, string ObjectName)
 {
@@ -32,7 +39,12 @@ void AssetLoader::LoadObject(vector<shared_ptr<T>>& Object, string ObjectName)
         ObjectFile.ObjectType(ObjectName);
         ObjectFile.ReadData2(Object);
 }
-
+/**
+ * @brief Template specialization for loading a shared_ptr object.
+ * @tparam T The type of the object being loaded.
+ * @param Object A shared pointer to an object of type T.
+ * @param ObjectName The name of the object to be loaded.
+ */
 template <typename T>
 void AssetLoader::LoadObject(shared_ptr<T>& Object, string ObjectName)
 {
@@ -45,6 +57,12 @@ void AssetLoader::LoadObject(shared_ptr<T>& Object, string ObjectName)
         Object = make_shared<T>();
 }
 
+/**
+ * @brief Template specialization for loading a unique_ptr object.
+ * @tparam T The type of the object being loaded.
+ * @param Object A unique pointer to an object of type T.
+ * @param ObjectName The name of the object to be loaded.
+ */
 template <typename T>
 void AssetLoader::LoadObject(unique_ptr<T>& Object, string ObjectName)
 {
