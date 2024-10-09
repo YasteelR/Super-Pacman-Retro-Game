cc1f90c2f4d7b7fb76596ca1bab9b78c48c2448c
Yasteel Ramphal <yasteel001@gmail.com>
2024-10-09 18:30:56 +0200

    documentation for file reader

--- game-source-code/FileReader.h
+++ game-source-code/FileReader.h
@@ -10,99 +10,126 @@
 
 using namespace std;
 
-class FileReader
-{
-    public:
-        FileReader(){};
-        FileReader(string ReadInFile);
+/**
+ * @brief Reads game objects from a file.
+ * 
+ * The FileReader class handles reading in different types of
+ * objects from a specified text file.
+ */
+class FileReader {
+public:
+    FileReader() {};
+    /**
+     * @brief Constructor that initializes the file to read.
+     * 
+     * @param ReadInFile The name of the file to read.
+     */
+    FileReader(string ReadInFile);
+    
+    /**
+     * @brief Sets the map file.
+     * 
+     * @param FileName The name of the file to set.
+     */
+    void SetMapFile(string FileName);
+    
+    /**
+     * @brief Opens the specified map file.
+     */
+    void OpenFile();
+    
+    /**
+     * @brief Opens the specified file.
+     * 
+     * @param FileName The name of the file to open.
+     */
+    void OpenFile(string FileName);
+    
+    /**
+     * @brief Closes the currently open file.
+     */
+    void CloseFile();
+    
+    /**
+     * @brief Checks if the file is open.
+     * 
+     * @return true if the file is open, false otherwise.
+     */
+    bool FileIsOpen();
+    
+    /**
+     * @brief Reads object types from the map file.
+     * 
+     * @param NameOfObject The name of the object type to read.
+     */
+    void ObjectType(string NameOfObject);
+    
+    /**
+     * @brief Reads data for objects with two coordinates.
+     * 
+     * @tparam T The type of object to read.
+     * @param StoreObjects The vector to store the read objects.
+     */
+    template <typename T>
+    void ReadData2(vector<shared_ptr<T>>& StoreObjects);
+    
+    /**
+     * @brief Reads data for objects with four coordinates.
+     * 
+     * @tparam T The type of object to read.
+     * @param StoreObjects The vector to store the read objects.
+     */
+    template <typename T>
+    void ReadData4(vector<shared_ptr<T>>& StoreObjects);
 
-        void SetMapFile(string FileName);
-        void OpenFile();
-        void OpenFile(string FileName);//Overload OpenFile to take in a file name
-        void CloseFile();
-        bool FileIsOpen();
-
-        //Reads the Map txt file.
-        void ObjectType(string NameOfObject); //The Name of the object i.e rectangles, lines, circles, etc...
-
-        //This is a standard template for reading in different objects from the text file.
-        //It's for objects that have two coordinates
-        template <typename T>
-        void ReadData2(vector<shared_ptr<T>> &StoreObjects);
-        template <typename T>
-        void ReadData4(vector<shared_ptr<T>> &StoreObjects);
-
-    private:
-        string MapFile;
-        ifstream InputFile;
-        string Object;
-
+private:
+    string MapFile; ///< The name of the map file.
+    ifstream InputFile; ///< Input file stream.
+    string Object; ///< Current object type being read.
 };
 
 template <typename T>
-void FileReader::ReadData2(vector<shared_ptr<T>>& StoreObjects)
-{
+void FileReader::ReadData2(vector<shared_ptr<T>>& StoreObjects) {
     static_assert(std::is_base_of<BaseObject, T>::value, "T must be derived from BaseObject");
     InputFile.seekg(0, std::ios::beg);
     string HeaderLine;
-    while (HeaderLine != Object && !InputFile.eof())
-    {
-
+    while (HeaderLine != Object && !InputFile.eof()) {
         getline(InputFile, HeaderLine);
     }
-    
     bool DataWasRead = false;
-    int xpos;
-    int ypos;
-    while (InputFile >> xpos >> ypos)
-    {
+    int xpos, ypos;
+    while (InputFile >> xpos >> ypos) {
         StoreObjects.emplace_back(make_shared<T>());
         StoreObjects.back()->set_location(xpos, ypos);
         DataWasRead = true;
     }
-
-    if (DataWasRead == false)
-    {
+    if (!DataWasRead) {
         throw runtime_error("File contained no coordinates");
     }
-    
     InputFile.clear();
-};
-
+}
 
 template <typename T>
-void FileReader::ReadData4(vector<shared_ptr<T>>& StoreObjects)
-{
+void FileReader::ReadData4(vector<shared_ptr<T>>& StoreObjects) {
     static_assert(std::is_base_of<BaseObject, T>::value, "T must be derived from BaseObject");
     InputFile.seekg(0, std::ios::beg);
     string HeaderLine;
-    while (HeaderLine != Object && !InputFile.eof())
-    {
-
+    while (HeaderLine != Object && !InputFile.eof()) {
         getline(InputFile, HeaderLine);
     }
-    
     bool DataWasRead = false;
-    int xpos;
-    int ypos;
-    int width;
-    int height;
-
-    while (InputFile >> xpos >> ypos >> width >> height)
-    {
+    int xpos, ypos, width, height;
+    while (InputFile >> xpos >> ypos >> width >> height) {
         StoreObjects.emplace_back(make_shared<T>());
         StoreObjects.back()->set_location(xpos, ypos);
         StoreObjects.back()->setWidth(width);
         StoreObjects.back()->setHeight(height);
         DataWasRead = true;
     }
-
-    if (DataWasRead == false)
-    {
+    if (!DataWasRead) {
         throw runtime_error("File contained no coordinates");
     }
-    
     InputFile.clear();
-};
+}
 
 #endif /* B5A97AA2_DF14_43C3_88A9_C49E15AF5231 */
